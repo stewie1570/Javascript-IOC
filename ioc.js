a621cc4122338be34ab283929904cf37a6faae61
Stewart Anderson <stewie1570@gmail.com>
2013-10-10 19:42:25 -0600

    Initial commit..

---
+++ ioc.js
@@ -0,0 +1,85 @@
+ioc =
+    {
+        registeredDependencies: [],
+        bindToConstructor: function (argName, construct)
+        {
+            var registration = {
+                argName: argName,
+                construct: construct
+            };
+            this.registeredDependencies.push(registration);
+        },
+        bindToConstant: function(argName, constant)
+        {
+            var registration = {
+                argName: argName,
+                constant: constant
+            };
+            this.registeredDependencies.push(registration);
+        },
+        get: function (construct)
+        {
+            var args = this.getArgNames(construct);
+            var self = this;
+            var depConstructsOrConsts = this.arraySelect(args, function (arg)
+            {
+                var dependency = self.arrayFirst(self.registeredDependencies, function (regDep)
+                {
+                    return regDep.argName == arg;
+                });
+                if (dependency == null)
+                    throw "Un-registered dependency '" + arg + "'.";
+                return dependency.construct || dependency.constant;
+            });
+            var constructedDependencies = this.arraySelect(depConstructsOrConsts, function (depConstructOrConst)
+            {
+                if (self.getArgNames(depConstructOrConst).length > 0)
+                    return self.get(depConstructOrConst);
+                else
+                    return typeof (depConstructOrConst) == "function"
+                        ? new depConstructOrConst() : depConstructOrConst;
+            });
+            return this.applyToConstructor(construct, constructedDependencies);
+        },
+        applyToConstructor: function (construct, argArray)
+        {
+            var args = [null].concat(argArray);
+            var factoryFunction = construct.bind.apply(construct, args);
+            return new factoryFunction();
+        },
+        arraySelect: function (arr, del)
+        {
+            var ret = [];
+            for (var i = 0; i < arr.length; i++)
+            {
+                ret.push(del(arr[i]));
+            }
+            return ret;
+        },
+        arrayFirst: function (arr, del)
+        {
+            for (var i = 0; i < arr.length; i++)
+            {
+                if (del(arr[i]))
+                    return arr[i];
+            }
+            return null;
+        },
+        getArgNames: function (construct)
+        {
+            var code = construct
+                .toString()
+                .replace(/\s/g, '');
+            var start = code.indexOf("(") + 1;
+            var end = code.indexOf(")");
+
+            if (start < 0 || end < 0 || end - start < 1)
+                return [];
+            else
+            {
+                return code
+                    .substring(start, end)
+                    .split(",");
+            }
+        }
+    };
---
+++ tests/Spec.js
@@ -0,0 +1,81 @@
+/// <reference path="jasmine.js" />
+/// <reference path="../ioc.js" />
+
+
+describe("Dependency Injector", function ()
+{
+    var Dependency = null;
+    var Implementation = null;
+
+    beforeEach(function ()
+    {
+        Dependency1 = function ()
+        {
+            this.prop1 = "success1";
+        };
+        Dependency2 = function ()
+        {
+            this.prop1 = "success2";
+        };
+        Implementation = function (dependency1, dependency2)
+        {
+            this.prop1 = dependency1.prop1;
+            this.prop2 = dependency2.prop1;
+        };
+        Implementation2 = function(impl)
+        {
+            this.success = impl.prop1;
+        };
+        ioc.bindToConstructor("impl", Implementation);
+        ioc.bindToConstructor("dependency1", Dependency1);
+        ioc.bindToConstructor("dependency2", Dependency2);
+        ioc.bindToConstant("constTest", "constant success");
+    });
+
+    it("should support nested dependencies", function()
+    {
+        expect(ioc.get(Implementation2).success).toBe("success1");
+    });
+
+    it("should inject dependencies by contstructor argument names", function ()
+    {
+        var impl = ioc.get(Implementation);
+        expect(impl.prop1).toBe("success1");
+        expect(impl.prop2).toBe("success2");
+    });
+
+    it("should be able to get constructor argument names", function ()
+    {
+        Constructor = function (arg1, arg2) { };
+        var argNames = ioc.getArgNames(Constructor);
+        expect(argNames).toEqual(["arg1", "arg2"]);
+    });
+
+    it("should know when constructors dont have dependencies", function ()
+    {
+        Constructor = function () { };
+        var argNames = ioc.getArgNames(Constructor);
+        expect(argNames).toEqual([]);
+    });
+
+    it("should throw exception for requested/un-registered dependencies", function ()
+    {
+        Constructor = function (unknownDep) { };
+        var exception = "";
+        try
+        {
+            ioc.get(Constructor);
+        }
+        catch (ex)
+        {
+            exception = ex;
+        }
+        expect(exception).not.toBe("");
+    });
+
+    it("should support binding to constants, not just constructors", function ()
+    {
+        Implementation = function (constTest) { this.prop1 = constTest; };
+        expect(ioc.get(Implementation).prop1).toBe("constant success");
+    });
+});
