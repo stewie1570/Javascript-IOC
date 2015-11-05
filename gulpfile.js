998a8d83f5a0b91f4787327f82238c3f97a907a6
Stewart Anderson <stewie1570@gmail.com>
2015-11-04 23:31:00 -0700

    "npm test" now does a single run of the tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 var gulp = require('gulp');
 var KarmaServer = require('karma').Server;
+var gutil = require('gulp-util');
+var path = require('path');
+var karma = require('karma');
+var karmaParseConfig = require('karma/lib/config').parseConfig;
 
 gulp.task('tdd', function (done) {
 	new KarmaServer({
 		configFile: __dirname + '/karma.conf.js'
 	}, done).start();
+});
+
+function runKarma(configFilePath, options, cb) {
+
+	configFilePath = path.resolve(configFilePath);
+
+	var server = karma.server;
+	var log=gutil.log, colors=gutil.colors;
+	var config = karmaParseConfig(configFilePath, {});
+
+    Object.keys(options).forEach(function(key) {
+      config[key] = options[key];
+    });
+
+	server.start(config, function(exitCode) {
+		log('Karma has exited with ' + colors.red(exitCode));
+		cb();
+		process.exit(exitCode);
+	});
+}
+
+gulp.task('test', function(done) {
+	runKarma('karma.conf.js', {
+		autoWatch: false,
+		singleRun: true
+	}, done);
 });
 
 gulp.task('default', ['tdd'], function (done) { });
