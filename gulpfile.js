976c2aa7591696c2556429d05721575dc06c57a9
Stewart Anderson <stewie1570@gmail.com>
2015-11-04 23:18:59 -0700

    Simplified the dev environment.

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,5 @@
 var gulp = require('gulp');
-var gulpWebpack = require('gulp-webpack');
-var webpack = require('webpack');
-var clean = require('gulp-clean');
 var KarmaServer = require('karma').Server;
-var less = require('gulp-less');
-var path = require('path');
-var watch = require('gulp-watch');
-var minifyCss = require('gulp-minify-css');
-
-function build(ops) {
-	var webpackConfig = require('./webpack.config.js');
-
-	webpackConfig.devtool = ops.sourceMap ? 'source-map' : undefined;
-	webpackConfig.watch = ops.watch;
-
-	return gulp.src('src/ioc.js')
-		.pipe(gulpWebpack(webpackConfig))
-		.pipe(gulp.dest('./'));
-}
-
-gulp.task('less', function () {
-	gulp.src('./styles/**/*.less')
-		.pipe(watch('styles/**/*.less'))
-		.pipe(less({
-			paths: [path.join(__dirname, 'less', 'includes')]
-		}))
-		.pipe(minifyCss())
-		.pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('clean', function () {
-	return gulp.src('public/app/**', { read: false }).pipe(clean());
-});
 
 gulp.task('tdd', function (done) {
 	new KarmaServer({
@@ -40,17 +8,3 @@ gulp.task('tdd', function (done) {
 });
 
 gulp.task('default', ['tdd'], function (done) { });
-
-gulp.task('build-dev', ['clean', 'less'], function () {
-	return build({
-		sourceMap: true,
-		watch: true
-	});
-});
-
-gulp.task('build-prod', ['clean', 'less'], function () {
-	return build({
-		sourceMap: false,
-		watch: false
-	});
-});
